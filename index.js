3a190546289755cd2515229e6312d80adec1d8be
Ajagbe Uthman <uthman4u2nv@yahoo.com>
2022-02-08 21:08:30 +0100

    Update index.js
    Included the Fixed deposit Endpoint

--- index.js
+++ index.js
@@ -33,6 +33,83 @@ app.get('/',(req,res)=>{
   res.send("Welcome to Baines Account Opening API");
 })
 
+function ReturnDateTime(){
+  let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+
+// prints date in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+
+// prints time in HH:MM format
+//console.log(hours + ":" + minutes);
+}
+
+app.post('/OpenFixedDepositAccount',async(req,res)=>{
+console.log(req);
+  const data=
+  {
+    "IsDiscountDeposit": true,
+    "Amount": req.body.Amount,
+    "InterestRate": req.body.InterestRate,
+    "Tenure": req.body.tenure,
+    "CustomerID": req.body.CustomerID,
+    "ProductCode": req.body.ProductCode,
+    "LiquidationAccount": req.body.LiquidationAccount,
+    "InterestAccrualCommenceDate": ReturnDateTime()
+  }
+const url='http://52.168.85.231/BankOneWebAPI/api/FixedDeposit/CreateFixedDepositAcct2/2?authtoken=60631a02-9cfe-40e1-949b-7b080d827955';
+axios.post(url, data)
+      .then((response) => {
+          
+          if(response.data.IsSuccessful){
+            let myResponse={
+                responseCode:"00",
+                responseMessage:response.data.Message
+                
+            }
+            res.send(JSON.stringify(myResponse));
+          }else{
+            let myResponse={
+                responseCode:"69",
+                responseMessage:response.data.Message,
+                
+                
+
+            }
+            res.send(JSON.stringify(myResponse));
+          }
+
+         
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+        res.send(JSON.stringify(error));
+      });
+
+
+})
+
 app.post('/OnlineApply',async(req,res)=>{
 
 const data=
